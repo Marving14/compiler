de9e302f251cda02d62d0312c863a36c661973b5
Marving14 <bryan_marving@hotmail.com>
2020-10-26 21:03:39 -0600

    lexer changes

--- basic.py
+++ basic.py
@@ -3,22 +3,53 @@
 # Some constants
 #########################################################
 DIGITS = '0123456789'
+LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+#ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 #########################################################
 # Errors
 #########################################################
 class Error:
-    def __init__(self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details 
     
     def as_string(self): 
         result = f'{self.error_name}: {self.details}'
+        result += f' File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
 
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Ilegal Character', details)
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, ' Ilegal Character', details)
+
+
+#########################################################
+# Position 
+#########################################################
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+    
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+
+        if(current_char == '\n'):
+            self.ln += 1
+            self.col = 0
+        
+        return self
+    
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
 
 #########################################################
 # Tokens
@@ -45,9 +76,10 @@ TT_         = ''
 #########################################################
 class Token:
     def __init__(self, type_, value=None):
+        self.type = type_
         self.value = value
 
-    def __rep__(self):
+    def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
 
@@ -55,19 +87,20 @@ class Token:
 # LEXER class
 #########################################################
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance() 
 
     def advance(self):
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance(self.current_char) 
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     def make_tokens(self):
         tokens = []
-        while self.current_char != None
+        while self.current_char != None: 
             if self.current_char in ' \t':
                 self.advance()
             elif self.current_char in DIGITS:
@@ -91,9 +124,10 @@ class Lexer:
                 tokens.append(Token(TT_RPARENT))
                 self.advance()
             else:
+                pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError("'" + char + "'")
+                return [], IllegalCharError(pos_start, self.pos,"'" + char + "'")
 
         return tokens, None
     
@@ -119,8 +153,14 @@ class Lexer:
 #########################################################
 # Run
 #########################################################
-def run(text):
-    lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens() 
 
     return tokens, error
+
+
+
+
+
+
--- shell.py
+++ shell.py
@@ -2,11 +2,12 @@ import basic
 
 while True: 
     text = input('command > ')
-    result, error = basic.run(text)
+    result, error = basic.run('<stdin>', text)
     
     if error: print(error.as_string())
     else: print(result)
     
+
 
 
 
