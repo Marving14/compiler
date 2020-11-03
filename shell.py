2c956e2a608516ca6599c8d7616557f586b1835e
Marving14 <bryan_marving@hotmail.com>
2020-11-03 00:12:39 -0600

    Functions now work and can be called

--- shell.py
+++ shell.py
@@ -11,4 +11,4 @@ while True:
 
 
 
-#https://www.youtube.com/watch?v=acCfcPXiX3A&list=PLZQftyCk7_SdoVexSmwy_tBgs7P0b97yD&index=8&ab_channel=CodePulse
+#https://www.youtube.com/watch?v=_Gb5H7lJSlE&list=PLZQftyCk7_SdoVexSmwy_tBgs7P0b97yD&index=10&ab_channel=CodePulse
