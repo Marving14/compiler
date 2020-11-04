1eba3f997c274fea00922e85a40f6f584c67f28c
Marving14 <bryan_marving@hotmail.com>
2020-11-04 02:04:25 -0600

    multi line

--- shell.py
+++ shell.py
@@ -2,13 +2,19 @@ import basic
 
 while True: 
     text = input('command > ')
+    if text.strip() =="": continue
     result, error = basic.run('<stdin>', text)
     
     if error: print(error.as_string())
-    elif result: print(result)
+    elif result: 
+        if len(result.elements) ==1:
+            print(repr(result.elements[0]))
+        else :
+            print(result)
  
 
 
 
 
 #https://www.youtube.com/watch?v=zKCckdwwBsU&list=PLZQftyCk7_SdoVexSmwy_tBgs7P0b97yD&index=13&ab_channel=CodePulse
+
